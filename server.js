085219562ce7ba9f8c745e4aa57b8622f7ee2f0e
Radhe1912 <106031414+Radhe1912@users.noreply.github.com>
2025-02-01 20:25:28 +0530

    Initial commit

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const connectDB = require('./db/db');
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({}));
+app.use(cors({
+    origin: 'https://expense-mania-tracker.vercel.app', // Allow your frontend to make requests to the backend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+    credentials: true, // Allow cookies to be sent with requests
+}));
 
 app.use(express.json());
 app.use(cookieParser());
